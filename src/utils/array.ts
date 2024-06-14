aa28cc3c067be14daba2e15b57c790eb9813ccff
bbush915 <36167099+bbush915@users.noreply.github.com>
2024-06-14 08:54:32 -0500

    Initial commit

---
+++ src/utils/array.ts
@@ -0,0 +1,75 @@
+declare global {
+  interface Array<T> {
+    /**
+     * Calculates the sum of an array of numbers.
+     *
+     * @param {number[]} this The array of values.
+     * @return {number} The sum, or 0 if the array is empty.
+     */
+    sum(this: number[]): number;
+
+    /**
+     * Calculates the product of an array of numbers.
+     *
+     * @param {number[]} this The array of values.
+     * @return {number} The product, or 1 if the array is empty.
+     */
+    product(this: number[]): number;
+  }
+}
+
+Object.defineProperty(Array.prototype, "sum", {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: function (this: number[]) {
+    return this.reduce((sum, value) => (sum += value), 0);
+  },
+});
+
+Object.defineProperty(Array.prototype, "product", {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: function (this: number[]) {
+    return this.reduce((product, value) => (product *= value), 1);
+  },
+});
+
+/**
+ * Determines whether the given value is an array.
+ *
+ * @param {*} value The value.
+ * @return {boolean} A value indicating whether the value is an array.
+ */
+export function isArray(value: any): boolean {
+  return Array.isArray(value);
+}
+
+/**
+ * Generates a range of value from [start, end).
+ *
+ * @param start The first value of the range.
+ * @param finish The last value of the range (not inclusive).
+ * @returns  The range of values.
+ */
+export function range(start: number, finish: number): number[] {
+  return Array.from({ length: finish - start }, (_, i) => i + start);
+}
+
+/**
+ * Returns the cartesian product of the given arrays.
+ *
+ * @template T
+ * @param {T[][]} arrays The arrays.
+ * @return {T[][]} The cartesian product of the given arrays.
+ */
+export function cartesian<T>(...arrays: T[][]): T[][] {
+  return arrays.reduce(
+    (productArrays, array) =>
+      productArrays.flatMap((productArray) =>
+        array.map((value) => [...productArray, value])
+      ),
+    [[]] as T[][]
+  );
+}
