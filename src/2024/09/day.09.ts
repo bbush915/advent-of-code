f1029273d9f9a02c09f30380f92a09af22fec624
bbush915 <bushb@umich.edu>
2024-12-09 00:35:06 -0600

    feat: Day 9

--- src/2024/09/day.09.ts
+++ src/2024/09/day.09.ts
@@ -1,95 +1,108 @@
 import fs from "fs";
 
-import "@/utils/array";
-
 function parseInput() {
   return fs
     .readFileSync("src/inputs/2024/09/day.09.input.txt")
     .toString()
-    .split("\n")
-    .filter((x) => x)
-    .map((x) => {
-      return x.split("").map(Number);
-    });
+    .split("")
+    .map(Number);
 }
 
 export function part1() {
-  const input = parseInput();
+  const diskMap = parseInput();
 
-  let total = 0;
+  // NOTE - Expand disk map.
 
-  for (const line of input) {
-    const filesystem: number[] = [];
+  const disk: number[] = [];
 
-    for (let i = 0; i < line.length; i += 2) {
-      for (let j = 0; j < line[i]; j++) {
-        filesystem.push(Math.floor(i / 2));
-      }
+  for (let i = 0; i < diskMap.length; i += 2) {
+    disk.push(
+      ...new Array(diskMap[i]).fill(Math.floor(i / 2)),
+      ...new Array(diskMap[i + 1] ?? 0).fill(-1)
+    );
+  }
 
-      for (let j = 0; j < (line[i + 1] ?? 0); j++) {
-        filesystem.push(-1);
-      }
-    }
+  // NOTE - Compact files.
 
-    let freeIndex = filesystem.findIndex((x) => x < 0);
-    let lastIndex = filesystem.length - 1;
+  let freeSpaceIndex = disk.findIndex((x) => x < 0);
 
-    while (freeIndex < lastIndex) {
-      filesystem.splice(freeIndex, 1, filesystem.splice(lastIndex, 1)[0]);
+  while (freeSpaceIndex < disk.length) {
+    disk.splice(freeSpaceIndex, 1, disk.pop()!);
 
-      while (freeIndex <= lastIndex && filesystem[freeIndex] !== -1) {
-        freeIndex++;
-      }
-
-      lastIndex--;
-    }
-
-    for (let i = 0; i < filesystem.length; i++) {
-      total += i * (filesystem[i] === -1 ? 0 : filesystem[i]);
+    while (disk[freeSpaceIndex] >= 0) {
+      freeSpaceIndex++;
     }
   }
 
-  return total;
+  // NOTE - Calculate checksum.
+
+  return disk.reduce(
+    (checksum, id, i) => (checksum += (id === -1 ? 0 : id) * i),
+    0
+  );
 }
 
 export function part2() {
-  const input = parseInput();
+  const diskMap = parseInput();
 
-  let total = 0;
+  // NOTE - Compute indices of files and free space on disk.
 
-  for (const line of input) {
-    const filesystem: number[] = [];
-
-    for (let i = 0; i < line.length; i += 2) {
-      for (let j = 0; j < line[i]; j++) {
-        filesystem.push(Math.floor(i / 2));
+  const { fileIndices, freeSpaceIndices } = diskMap.reduce<{
+    diskSize: number;
+    fileIndices: number[][];
+    freeSpaceIndices: number[][];
+  }>(
+    (acc, val, i, arr) => {
+      if (i % 2) {
+        return acc;
       }
 
-      for (let j = 0; j < (line[i + 1] ?? 0); j++) {
-        filesystem.push(-1);
+      const { fileIndices, freeSpaceIndices } = acc;
+
+      const fileId = Math.floor(i / 2);
+      const fileSize = val;
+
+      fileIndices.push([acc.diskSize, fileId, fileSize]);
+      acc.diskSize += fileSize;
+
+      const freeSpaceSize = arr[i + 1];
+
+      freeSpaceIndices.push([acc.diskSize, freeSpaceSize]);
+      acc.diskSize += freeSpaceSize;
+
+      return acc;
+    },
+    { diskSize: 0, fileIndices: [], freeSpaceIndices: [] }
+  );
+
+  // NOTE - Compact files.
+
+  for (let n = fileIndices.length - 1; n > 0; n--) {
+    const freeSpaceIndexIndex = freeSpaceIndices.findIndex(
+      (x) => x[0] < fileIndices[n][0] && x[1] >= fileIndices[n][2]
+    );
+
+    if (freeSpaceIndexIndex >= 0) {
+      const freeSpaceIndex = freeSpaceIndices[freeSpaceIndexIndex];
+
+      fileIndices[n][0] = freeSpaceIndex[0];
+
+      freeSpaceIndex[0] += fileIndices[n][2];
+      freeSpaceIndex[1] -= fileIndices[n][2];
+
+      if (!freeSpaceIndex[1]) {
+        freeSpaceIndices.splice(freeSpaceIndexIndex, 1);
       }
-    }
-
-    for (let n = Math.floor(line.length / 2); n > 0; n--) {
-      const count = filesystem.filter((x) => x == n).length;
-      const index = filesystem.findIndex((x) => x === n);
-
-      for (let i = 0; i < index; i++) {
-        if (filesystem.slice(i, i + count).every((x) => x === -1)) {
-          filesystem.splice(
-            i,
-            count,
-            ...filesystem.splice(index, count, ...new Array(count).fill(-1))
-          );
-          break;
-        }
-      }
-    }
-
-    for (let i = 0; i < filesystem.length; i++) {
-      total += i * (filesystem[i] === -1 ? 0 : filesystem[i]);
     }
   }
 
-  return total;
+  // NOTE - Calculate checksum.
+
+  return fileIndices.reduce((checksum, [index, fileId, fileSize]) => {
+    for (let i = 0; i < fileSize; i++) {
+      checksum += fileId * (index + i);
+    }
+
+    return checksum;
+  }, 0);
 }
